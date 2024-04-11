e98bcf09d37452bff922ccc84e9c9934672378ba
forwho <hwjbupt@gmail.com>
2024-04-11 21:43:17 +0100

    init

---
+++ data_organize.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import re
+
+def dcm2nii_dlb(spath,tpath):
+    subs=os.listdir(spath)
+    for sub in subs:
+        files=glob(["%s/%s/*/*" % (spath,sub)])
+        for file in files:
+            date=re.search("\d{4}-\d{2}-\d{2}_\d{2}_\d{2}",file,).group()
+            os.makedirs("%s/%s_%s/anat" % (tpath,sub,date))
+            cmd="dcm2niix -f t1.nii.gz -o %s/%s_%s/anat/ %s" % (tpath,sub,date,file)
+            os.system(cmd)
+
+if __name__=="__main__":
+    spath=""
+    tpath=""
+    dcm2nii_dlb(spath, tpath)
