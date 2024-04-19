bd77261b2ba16ca4c2597dea4268822addd7e704
md1weih <md1weih@login2.pri.stanage.alces.network>
2024-04-19 10:53:56 +0100

    global pib

--- data_organize.py
+++ data_organize.py
@@ -18,10 +18,14 @@ def dcm2nii_dlb(spath,tpath):
 def global_pib(spath,tpath,mask):
     image_data=nib.load(spath).get_fdata()
     mask_data=nib.load(mask).get_fdata()
-    suvr=np.nanmean(image_data[mask_data==1,:])
+    suvr=np.mean(image_data[mask_data==1,:],axis=0)
     np.savetxt(tpath,suvr)
 
 if __name__=="__main__":
-    spath="/mnt/parscratch/users/md1weih/dlb/data/ADNI"
-    tpath="/mnt/parscratch/users/md1weih/dlb/data/nifti"
-    dcm2nii_dlb(spath, tpath)
+    spath="/mnt/parscratch/users/md1weih/dlb/data/pet_stat_data/"
+    tpath="/mnt/parscratch/users/md1weih/dlb/data/pet_stat_data/"
+    mask="/mnt/parscratch/users/md1weih/dlb/data/pet_stat_data/global_pib_crop.nii.gz"
+    global_pib("%s/nc.nii.gz" % spath, "%s/nc.txt" % spath,mask)
+    global_pib("%s/ad.nii.gz" % spath, "%s/ad.txt" % spath,mask)
+    global_pib("%s/dlb.nii.gz" % spath, "%s/dlb.txt" % spath,mask)
+    #dcm2nii_dlb(spath, tpath)
